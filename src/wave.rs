2b8f35608bfa62a6226650566886cfe24b9e0557
konyogony <me@konyogony.dev>
2026-01-08 22:07:33 +0700

    Add the Phillips Spectrum function

--- src/wave.rs
+++ src/wave.rs
@@ -1,6 +1,13 @@
+use std::f32;
+
+use cgmath::{InnerSpace, Vector2};
 use rand::Rng;
 
+const WIND_VECTOR: Vector2<f32> = Vector2::new(-2.1, 2.8); // Magnitude of 3.5
+const AMPLITUDE: f32 = 0.0000001;
+
 use crate::state::WAVE_NUMBER;
+use rand_distr::{Distribution, Normal};
 
 #[repr(C)]
 #[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
@@ -8,6 +15,9 @@ pub struct WaveData {
     wave_vector: [f32; 2],
     amplitude: f32,
     phase_shift: f32,
+    xi_r: f32,
+    xi_i: f32,
+    phk: f32,
 }
 
 #[repr(C)]
@@ -30,13 +40,48 @@ impl WaveData {
         let theta = rng.random_range(0.0..std::f32::consts::TAU);
         let wavelength = rng.random_range(2.0..60.0);
         let k = std::f32::consts::TAU / wavelength;
+        let k1 = theta.cos() * k;
+        let k2 = theta.sin() * k;
+        let wave_vector = [k1, k2];
         let amplitude = wavelength * rng.random_range(0.004..0.012);
 
+        let normal = Normal::new(0.0, 1.0).unwrap();
+        let xi_r = normal.sample(&mut rand::rng()); // Random values from the gaussian distribution
+        let xi_i = normal.sample(&mut rand::rng());
+        let phk = WaveData::get_phillips_spectrum_value(wave_vector);
+
         WaveData {
-            wave_vector: [theta.cos() * k, theta.sin() * k],
+            wave_vector,
             amplitude,
             phase_shift: rng.random_range(0.0..std::f32::consts::TAU),
+            xi_r,
+            xi_i,
+            phk,
         }
+    }
+
+    // Get the phillips spectrum value by a complicated equation
+    pub fn get_phillips_spectrum_value(wave_vector: [f32; 2]) -> f32 {
+        let k: Vector2<f32> = wave_vector.into();
+        let k2 = k.magnitude2();
+        if k2 == 0.0 {
+            return 0.0;
+        }
+
+        let k_hat = k.normalize();
+
+        let w = WIND_VECTOR;
+        let w_hat = WIND_VECTOR.normalize();
+
+        let align = cgmath::dot(k_hat, w_hat).max(0.0);
+        let align2 = align * align;
+
+        let l = w.magnitude2() / 9.81;
+        let l2 = l * l;
+        let k4 = k2 * k2;
+        let exp = f32::exp(-1.0 / (k2 * l2));
+
+        (align2 * AMPLITUDE * exp) / k4
     }
 }
 
